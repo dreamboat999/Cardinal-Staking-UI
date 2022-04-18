d06e93feb66fd53fdb414f0bd34f88236ae93f31
Kyle Yutatka <kyleyutaka@gmail.com>
2022-04-17 18:17:04 -0700

    Added landing page, portals config saved

--- api/mapping.ts
+++ api/mapping.ts
@@ -5,4 +5,9 @@ export const poolMapping: [{ name: string; pool: PublicKey }] = [
     name: 'cardinal',
     pool: new PublicKey('A1h3AEScGehWAWdm3Q7uob8MGnhhkbB7FvH9gGhdzPC3'),
   },
+  {
+    name: 'portals',
+    displayName: 'Portals',
+    pool: new PublicKey('6H492AzY8eAS3VNjP7JpyurxCg17RH9wocBNfx4FAA6Q'),
+  },
 ]
