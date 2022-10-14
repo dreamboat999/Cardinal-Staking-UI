1000a3f0e94ab69a6d840e47b45a344bd929145e
Kyle Yutatka <kyleyutaka@gmail.com>
2022-10-14 18:28:13 +0300

    added hostname for meta collections

--- api/mapping.ts
+++ api/mapping.ts
@@ -29,6 +29,7 @@ export const poolMapping: [{ name: string; pool: PublicKey }] = [
     stakePoolAddress: new PublicKey(
       'BCtcLrKhiZbFTRMB2W8iQWttYF82cLJzo7ZnnnkqXnnC'
     ),
+    hostname: 'https://stake.metaladsai.com',
     websiteUrl?: 'https://metaladsai.com',
     imageUrl:
       'https://raw.githubusercontent.com/poisonlab/photo/main/WhatsApp%20Image%202022-10-05%20at%2017.53.36.jpeg',
